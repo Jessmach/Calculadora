52f378654dc697775bae5585a0e899a33f0c3de8
Jessica Machado <Jessica Machado@DESKTOP-IBO72AN>
2023-01-25 16:32:05 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int soma = 10 + 20;
+
+int soma2 = 1 - 3;
+
+int multi = 5 * 8;
+
+int divi = 1 / 1;
+
+Console.WriteLine($"soma = {soma} " );
+Console.WriteLine($"soma2 = {soma2} " );
+Console.WriteLine($"multi = {multi} " );
+Console.WriteLine($"divi = {divi}" );
